1dff7e3b51bfd2a4236b7b8c795ef16709c039df
Cooper Weinhold <cjweinhold@mail.fhsu.edu>
2024-09-10 14:45:36 -0500

    Added code to look at daily high of current day.

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 ### INF601 - Advanced Programming in Python
 ### Cooper Weinhold
 ### Miniproject 1
+import pprint
+import yfinance as yf
+myticker = ["MSFT", "AAPL", "NVDA", "GME", "LMT"]
+
+mydata = {}
+
+myticker.sort()
+for ticker in myticker:
+    result = yf.Ticker(ticker)
+    mydata[ticker] = {'ticker': ticker,
+                      'dayHigh': result.info['dayHigh']}
+
+pprint.pprint(mydata)
+
+#aapl = yf.Ticker("AAPL")
+
+# get all stock info
+#pprint.pprint(msft.info)
+
+# get historical market data
+#hist = msft.history(period="1mo")
+
+#pprint.pprint(hist)
